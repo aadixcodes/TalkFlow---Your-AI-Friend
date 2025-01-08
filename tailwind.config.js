86526f33a6834a2d5b9f69f3fcce7d7bea37bce8
iampateladitya <adityapatel77045@gmail.com>
2025-01-08 19:19:55 +0530

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,30 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+//   theme: {
+//     extend: {
+//       typography: {
+//         DEFAULT: {
+//           css: {
+//             color: '#fff',
+//             a: {
+//               color: '#8F2BC3',
+//               '&:hover': {
+//                 color: '#895DCA',
+//               },
+//             },
+//           },
+//         },
+//       },
+//     },
+//   },
+//   plugins: [
+//     require('@tailwindcss/typography'),
+//   ],
+// };
+
+import typography from '@tailwindcss/typography';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
@@ -19,6 +46,6 @@ export default {
     },
   },
   plugins: [
-    require('@tailwindcss/typography'),
+    typography,
   ],
 };
